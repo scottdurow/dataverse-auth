e3c8a6c3a74c960c4547655c9fb8ead21ba13d6e
Scott Durow <scott_durow@hotmail.com>
2020-04-15 16:16:15 +0100

    Initial Commit

---
+++ src/cds-auth.ts
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const electron = require("electron/");
+
+import proc from "child_process";
+
+// Get the tenant and env url from the arguments
+console.log(process.argv);
+if (process.argv.length < 4) {
+  console.log(
+    "Please supply the tenant (e.g. contoso.onmicrosoft.com) and CDS envrionment url (e.g. contoso-env.crm11.dynamics.com) ",
+  );
+  process.exit();
+} else {
+  // eslint-disable-next-line @typescript-eslint/no-use-before-define
+  main();
+}
+
+function main(): void {
+  const currentDir = __dirname;
+  const child = proc.spawn(electron, [currentDir + "/.", process.argv[2], process.argv[3]], {
+    stdio: "inherit",
+    windowsHide: false,
+  });
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  child.on("close", function(code: any) {
+    process.exit(code);
+  });
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const handleTerminationSignal = function(signal: any) {
+    process.on(signal, function signalHandler() {
+      if (!child.killed) {
+        child.kill(signal);
+      }
+    });
+  };
+
+  handleTerminationSignal("SIGINT");
+  handleTerminationSignal("SIGTERM");
+}
